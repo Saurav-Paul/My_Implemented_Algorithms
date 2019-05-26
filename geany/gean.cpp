5de0604b844a4fae6e678a857bbcf57cf9287b28
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-26 12:53:18 +0600

    My_template

--- geany/gean.cpp
+++ geany/gean.cpp
@@ -2,29 +2,46 @@
 #define endl '\n'
 #define ll long long int
 #define loop(i,a,b)           for(ll i=a;i<=b;++i)
-#define pb                    push_back
-#define F                     first
-#define S                     second
+#define boost				  ios_base::sync_with_stdio(0);
+#define eb                    emplace_back
 #define mp                    make_pair
 #define clr(x)                x.clear()
-#define MOD                   1000000007
+#define MOD                   1e9+7
 #define itoc(c)               ((char)(((int)'0')+c))
-#define vl                    vector<ll>
-#define SZ(x)                 (x).size()
+#define ctoi(c)               ((int)(((int)c)-'0'))
 #define all(p)                p.begin(),p.end()
-#define mid(s,e)              (s+(e-s)/2)
-#define sv()                  ll t,n; scanf("%lld",&t);n=t; while(t--)
-#define tcase()               ll t,n; cin>>t;n=t; while(t--)
 #define iscn(num)             scanf("%d",&num);
 #define scn(num)              scanf("%lld",&num);
 #define scn2(num,num2)        scanf("%lld%lld",&num,&num2);
 #define dbg(x)                cerr <<"( "<<#x << " = " << x << " )"<<endl
 using namespace std;
+template<class T >T set_bit(T N,T pos){return N=N | (1<<pos);}
+template<class T >T reset_bit(T N,T pos){return N= N & ~(1<<pos);}
+template<class T > bool check_bit(T N,T pos){return (bool)(N & (1<<pos));}
+template <class T > void max_self(T &a, T b){
+	a=max(a,b);
+}
+template <class T > void min_self(T &a, T b){
+	a=min(a,b);
+}
+////////////////////////
+
 const ll INF = 2e18 + 99;
 typedef pair<ll,ll> Pair;
-typedef vector<ll> vec;;
-void FAST_IO();
-void ara_read(vec &v,ll n);
+typedef vector<int> vec;;
+
+void ara_read(vec &v,ll n){
+	ll temp;
+	for(ll i=0; i< n; i++){
+		scanf("%lld",&temp);
+		v.emplace_back(temp);
+	}
+}
+void ara_print(vec &v){
+	for(ll x : v)
+		printf("%lld ",x);
+	puts("");
+}
 ////////////////////////
 
 int main()
@@ -33,22 +50,4 @@ int main()
 	
 	
     return 0;
-}
-
-void FAST_IO()
-{
-    // ios_base::sync_with_stdio(0);
-    //cin.tie(NULL);
-    //cout.tie(NULL);
-    //cout.setf(ios::fixed);
-    //cout.precision(2);
-
-}
-void ara_read(vec &v,ll n){
-	ll temp;
-	for(ll i=0; i<n; i++){
-		scanf("%lld",&temp);
-		v.emplace_back(temp);
-	}
-	
 }
