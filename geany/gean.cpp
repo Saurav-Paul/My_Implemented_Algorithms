350b6eaa4976baa5766de18cd5d39e870e2ce974
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-05-30 17:50:27 +0600

    Update gean.cpp

--- geany/gean.cpp
+++ geany/gean.cpp
@@ -1,3 +1,4 @@
+						/*Saurav Paul*/
 #include<bits/stdc++.h>
 #define endl '\n'
 #define ll long long int
@@ -13,41 +14,77 @@
 #define iscn(num)             scanf("%d",&num);
 #define scn(num)              scanf("%lld",&num);
 #define scn2(num,num2)        scanf("%lld%lld",&num,&num2);
-#define dbg(x)                cerr <<"( "<<#x << " = " << x << " )"<<endl
 using namespace std;
-template<class T >T set_bit(T N,T pos){return N=N | (1<<pos);}
-template<class T >T reset_bit(T N,T pos){return N= N & ~(1<<pos);}
-template<class T > bool check_bit(T N,T pos){return (bool)(N & (1<<pos));}
-template <class T > void max_self(T &a, T b){
+///////////////////////
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef PAUL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define dbg(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+////////////////////////
+
+template<typename T>T set_bit(T N,T pos){return N=N | (1<<pos);}
+template<typename T>T reset_bit(T N,T pos){return N= N & ~(1<<pos);}
+template<typename T> bool check_bit(T N,T pos){return (bool)(N & (1<<pos));}
+template<typename T>void bin_print(T N){bitset<25>bit(N);cerr<<bit.to_string()<<"\n";}
+template<typename T> void max_self(T &a, T b){
 	a=max(a,b);
 }
-template <class T > void min_self(T &a, T b){
+template <typename T > void min_self(T &a, T b){
 	a=min(a,b);
+}
+template <typename T > void add_self(T &a, T b){
+	a+=b;
 }
 ////////////////////////
 
-const ll INF = 2e18 + 99;
-typedef pair<ll,ll> Pair;
-typedef vector<int> vec;;
-
-void ara_read(vec &v,ll n){
+template<typename T> void ara_read(T &v,ll n){
 	ll temp;
 	for(ll i=0; i< n; i++){
 		scanf("%lld",&temp);
 		v.emplace_back(temp);
 	}
 }
-void ara_print(vec &v){
+template<typename T> void ara_print(T &v){
 	for(ll x : v)
 		printf("%lld ",x);
 	puts("");
 }
 ////////////////////////
 
+const ll INF = 2e18 + 99;
+typedef pair<ll,ll> Pair;
+typedef vector<ll> vec;;
+
 int main()
 {
 	
 	
+
 	
     return 0;
 }
