c49f18460469d852cda8ea2c4c853bba1032811e
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-10-15 19:03:23 +0600

    NOD

--- Number theory/Number of divisor ( NOD ) .cpp
+++ Number theory/Number of divisor ( NOD ) .cpp
@@ -5,7 +5,7 @@
    from there we can calculate NOD = (a1+1) * (a2+1) * (a3+1) .....
 */
 
-long long num_of_divisor(long long n){
+long long number_of_divisor(long long n){
     long long cnt = 1;
     for(auto x : primelist){
         if(x*x > n)
