ca0643f0a5f397c72c8b08e28c136b8df53f02f0
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2019-10-19 01:48:17 +0600

    Modular Inverse

---
+++ Number theory/BigMod_Inverse(only work in case of mod is a prime number ) .cpp
@@ -0,0 +1,18 @@
+ll BigMod(ll num , ll power, ll mod){
+	
+	if(power == 0 ){
+		return 1 ;
+	}
+	ll x = 1 ;
+	if(power&1){
+		x = num ;
+	}
+	ll y = BigMod(num,power/2,mod) ;
+	y = ( y%mod * y%mod )%mod ;
+	return (y%mod * x%mod)%mod ;
+	
+}
+ll BigModInverse(ll num , ll mod ){
+
+	return BigMod(num,mod-2,mod) ;
+}
