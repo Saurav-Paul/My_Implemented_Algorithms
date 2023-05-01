f9a6d8821618a5e1f7ed876c20100d3b1412e68e
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2023-05-01 21:08:30 +0600

    Create DSU.py

---
+++ python/DSU.py
@@ -0,0 +1,23 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        xset, yset = self.find(x), self.find(y)
+        if xset == yset:
+            return
+        
+        if self.rank[xset] < self.rank[yset]:
+            self.parent[xset] = yset
+        elif self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+        else:
+            self.parent[xset] = yset
+            self.rank[yset] += 1
+        
